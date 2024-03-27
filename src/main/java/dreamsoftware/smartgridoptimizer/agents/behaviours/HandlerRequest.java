37e48649d3979d41626ab71f917d13b2f59ae8e3
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2024-03-27 16:40:57 +0100

    Add doc

--- src/main/java/dreamsoftware/smartgridoptimizer/agents/behaviours/HandlerRequest.java
+++ src/main/java/dreamsoftware/smartgridoptimizer/agents/behaviours/HandlerRequest.java
@@ -9,22 +9,34 @@ import jade.domain.FIPAAgentManagement.RefuseException;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 import jade.proto.AchieveREResponder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import java.io.Serial;
+
+/**
+ * The HandlerRequest class is an abstract class that extends AchieveREResponder and provides a template for handling requests.
+ */
 public abstract class HandlerRequest extends AchieveREResponder {
-    
-    private Logger logger = LoggerFactory.getLogger(HandlerRequest.class);
-	
+
+	@Serial
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Constructs a HandlerRequest object with the specified agent and message template.
+	 * @param a the agent.
+	 * @param mt the message template.
+	 */
 	public HandlerRequest(Agent a, MessageTemplate mt) {
 		super(a, mt);
 	}
 
+	/**
+	 * Handles the request message received by the agent.
+	 * @param request the request message.
+	 * @return an agree message if the request is valid.
+	 * @throws RefuseException if the request is invalid or cannot be understood.
+	 */
 	@Override
-	protected ACLMessage handleRequest(ACLMessage request) throws NotUnderstoodException, RefuseException {
-
+	protected ACLMessage handleRequest(ACLMessage request) throws RefuseException {
 		try {
 			ContentElement ce = myAgent.getContentManager().extractContent(request);
 			if(!isValidRequest(ce))
@@ -39,6 +51,13 @@ public abstract class HandlerRequest extends AchieveREResponder {
 		}
 	}
 
+	/**
+	 * Prepares the result notification message to be sent after handling the request.
+	 * @param request the request message.
+	 * @param response the response message.
+	 * @return an inform message with the result of handling the request.
+	 * @throws FailureException if an error occurs while preparing the result notification.
+	 */
 	@Override
 	protected ACLMessage prepareResultNotification(ACLMessage request, ACLMessage response)
 			throws FailureException {
@@ -54,10 +73,18 @@ public abstract class HandlerRequest extends AchieveREResponder {
 			throw new FailureException("Error occurred while storing power");
 		}
 	}
-	
-	
+
+	/**
+	 * Checks if the request is valid.
+	 * @param ce the content element of the request.
+	 * @return true if the request is valid, false otherwise.
+	 */
 	protected abstract boolean isValidRequest(ContentElement ce);
-	
+
+	/**
+	 * Generates the response to the request.
+	 * @param requestContent the content element of the request.
+	 * @return the response predicate.
+	 */
 	protected abstract Predicate onGenerateResponse(ContentElement requestContent);
-	
 }
--- src/main/java/dreamsoftware/smartgridoptimizer/agents/impl/BatteryAgent.java
+++ src/main/java/dreamsoftware/smartgridoptimizer/agents/impl/BatteryAgent.java
@@ -1,5 +1,6 @@
 package dreamsoftware.smartgridoptimizer.agents.impl;
 
+import java.io.Serial;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -24,11 +25,16 @@ import dreamsoftware.smartgridoptimizer.ontology.predicates.StoreInBattery;
 import dreamsoftware.smartgridoptimizer.ontology.visitable.IBatteryVisitable;
 import dreamsoftware.smartgridoptimizer.ontology.visitor.IBatteryVisitor;
 
+/**
+ * The BatteryAgent class represents an agent responsible for storing and retrieving energy within the energy management system.
+ * It extends the PublishSubscribeAgent class and implements the IBatteryVisitor interface.
+ */
 public final class BatteryAgent extends PublishSubscribeAgent implements IBatteryVisitor {
 
+	@Serial
 	private static final long serialVersionUID = 1L;
 	
-	private Logger logger = LoggerFactory.getLogger(BatteryAgent.class);
+	private final Logger logger = LoggerFactory.getLogger(BatteryAgent.class);
 	
 	public static final String AGENT_NAME = "BatteryAgent";
 	public final static String AGENT_DESCRIPTION = "Agents that store energy";
@@ -38,8 +44,9 @@ public final class BatteryAgent extends PublishSubscribeAgent implements IBatter
 	
 	private Double pbat = 0.0;
 	
-	private HandlerRequest handlerRequest = new HandlerRequest(this, fipaRequestTemplate) {
+	private final HandlerRequest handlerRequest = new HandlerRequest(this, fipaRequestTemplate) {
 		
+		@Serial
 		private static final long serialVersionUID = 1L;
 
 		@Override
@@ -54,13 +61,16 @@ public final class BatteryAgent extends PublishSubscribeAgent implements IBatter
 		}
 	};
 
+	/**
+	 * Initializes the BatteryAgent by setting up behaviors and registering services.
+	 */
 	@Override
 	protected void setup() {
 		super.setup();
 		this.addBehaviour(handlerRequest);
 		// Tick Behaviour to periodically report your battery level.
 		this.addBehaviour(new TickerBehaviour(this, NOTIFY_INTERVAL) {
-			
+			@Serial
 			private static final long serialVersionUID = 1L;
 
 			@Override
@@ -71,7 +81,11 @@ public final class BatteryAgent extends PublishSubscribeAgent implements IBatter
 			}
 		});
 	}
-	
+
+	/**
+	 * Registers services provided by the BatteryAgent.
+	 * @param description the agent's description.
+	 */
 	@Override
 	protected void onRegisterServices(DFAgentDescription description) {
 		ServiceDescription service = new ServiceDescription();
@@ -81,7 +95,9 @@ public final class BatteryAgent extends PublishSubscribeAgent implements IBatter
 	}
 
 	/**
-	 * Process a StoreInBattery FIPA-Request
+	 * Processes a StoreInBattery request.
+	 * @param storeInBattery the request.
+	 * @return the response predicate.
 	 */
 	public Predicate visit(StoreInBattery storeInBattery) {
 		logger.debug("Storage " + storeInBattery.getSurplus().getSurplusValue() + " in battery " + this.getName());
@@ -91,7 +107,9 @@ public final class BatteryAgent extends PublishSubscribeAgent implements IBatter
 	}
 
 	/**
-	 * Process a RetrieveDemand FIPA-Request
+	 * Processes a RetrieveDemand request.
+	 * @param retrieveDemand the request.
+	 * @return the response predicate.
 	 */
 	public Predicate visit(RetrieveDemand retrieveDemand) {
 		logger.debug("Retrieve " + retrieveDemand.getSurplus().getSurplusValue() + " from battery " + this.getName());
@@ -100,18 +118,30 @@ public final class BatteryAgent extends PublishSubscribeAgent implements IBatter
 		return new NotifierBatteryLevel(getAID(), new BatteryLevel(pbat));
 	}
 
+	/**
+	 * Processes a GetStatus request.
+	 * @param getStatus the request.
+	 * @return the response predicate.
+	 */
+	public Predicate visit(GetStatus getStatus) {
+		return new CurrentBatteryLevel(getAID(), new BatteryLevel(pbat));
+	}
+
+	/**
+	 * Sets up JMX MBean attributes for BatteryAgent.
+	 * @param resourceInfo the resource information.
+	 * @param attributeFieldInfoList the list of attribute field information.
+	 * @param attributeMethodInfoList the list of attribute method information.
+	 * @param operationInfoList the list of operation information.
+	 */
 	@Override
-	protected void onCreateMBean(JmxResourceInfo resourceInfo, List<JmxAttributeFieldInfo> attributeFieldInfos,
-			List<JmxAttributeMethodInfo> attributeMethodInfos, List<JmxOperationInfo> operationInfos) {
+	protected void onCreateMBean(JmxResourceInfo resourceInfo, List<JmxAttributeFieldInfo> attributeFieldInfoList,
+			List<JmxAttributeMethodInfo> attributeMethodInfoList, List<JmxOperationInfo> operationInfoList) {
 		
 		resourceInfo.setJmxDomainName(AGENT_NAME);
 		resourceInfo.setJmxBeanName(this.getAID().getLocalName());
 		resourceInfo.setJmxDescription(AGENT_DESCRIPTION);
 		
-		attributeFieldInfos.add(new JmxAttributeFieldInfo("CELL_MAX_BAT_LEVEL", true, true, "Cell Max Battery Level for all Battery Agents"));
-	}
-
-	public Predicate visit(GetStatus getStatus) {
-		return new CurrentBatteryLevel(getAID(), new BatteryLevel(pbat));
+		attributeFieldInfoList.add(new JmxAttributeFieldInfo("CELL_MAX_BAT_LEVEL", true, true, "Cell Max Battery Level for all Battery Agents"));
 	}
 }
